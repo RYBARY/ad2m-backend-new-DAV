f904ee493bb25a0971fb7ea0e60b57913b85b069
RYBARY <ribarisoabarthelemy@gmail.com>
2026-01-23 22:19:34 +0000

    test

--- vite.config.js
+++ vite.config.js
@@ -1,40 +1,20 @@
 import { defineConfig } from 'vite'
 import laravel from 'laravel-vite-plugin'
 import vue from '@vitejs/plugin-vue'
-import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
     laravel({
-      input: [
-        'resources/css/app.css',
-        'resources/js/app.js',
-      ],
+      input: ['resources/css/app.css', 'resources/js/app.js'],
       refresh: true,
     }),
     vue(),
-    tailwindcss(),
   ],
 
   server: {
-  host: true,
-  port: 5173,
-  strictPort: true,
-  cors: true, 
-
-  // ✅ IMPORTANT: URL publique Codespaces (sinon public/hot devient localhost / 0.0.0.0)
-  origin: 'https://bug-free-memory-r4vgp7gw9vgqfp6v6-5173.app.github.dev',
-
-  // ✅ HMR via HTTPS (Codespaces)
-  hmr: {
-    protocol: 'wss',
-    host: 'bug-free-memory-r4vgp7gw9vgqfp6v6-5173.app.github.dev',
-    clientPort: 443,
+    host: '0.0.0.0',
+    port: 5173,
+    strictPort: true,
+    cors: true,
   },
-
-  watch: {
-    usePolling: true,
-  },
-},
-
 })
